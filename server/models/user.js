f8f4fbd6b189de1daf97a4f424aa30b288514fae
DerfMongol <leefredmart@gmail.com>
2018-12-17 19:33:22 -0500

    hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
+const bcrypt = require('bcryptjs')
 
 var UserSchema = new mongoose.Schema({
     email: {
         required: true,
         trim: true,
-        type: String, 
+        type: String,
         minlength: 1,
         unique: true,
         validate: {
@@ -16,7 +17,7 @@ var UserSchema = new mongoose.Schema({
         }
     },
     password: {
-        type: String, 
+        type: String,
         require: true,
         minlength: 6
     },
@@ -26,7 +27,7 @@ var UserSchema = new mongoose.Schema({
             required: true
         },
         token: {
-            type: String, 
+            type: String,
             required: true
         }
     }]
@@ -35,23 +36,23 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.toJSON = function () {
     let user = this
     let userObject = user.toObject()
-    
+
     return _.pick(userObject, ['_id', 'email'])
 }
 
-UserSchema.methods.generateAuthToken = function() {
+UserSchema.methods.generateAuthToken = function () {
     let user = this
     let access = 'auth'
-    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+    let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString()
 
-    user.tokens = user.tokens.concat([{access, token}])
+    user.tokens = user.tokens.concat([{ access, token }])
 
     return user.save().then(() => {
         return token
     })
 }
 
-UserSchema.statics.findByToken = function(token) {
+UserSchema.statics.findByToken = function (token) {
     let User = this
     let decoded
 
@@ -68,6 +69,21 @@ UserSchema.statics.findByToken = function(token) {
     })
 }
 
+UserSchema.pre('save', function (next) {
+    let user = this
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
 var User = mongoose.model('User', UserSchema)
 
-module.exports = {User}
+module.exports = { User }
